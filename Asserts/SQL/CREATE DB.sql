c2a4127f5f0dde1ab1d74057a8c10aa6beebfb93
Daniel Massoudyan <massoudyan@hotmail.com>
2020-09-08 22:12:02 +0200

    sql fix
    changes in sql file

--- Asserts/SQL/CREATE DB.sql
+++ Asserts/SQL/CREATE DB.sql
@@ -1,58 +1,69 @@
 DROP DATABASE IF EXISTS DatingAppH2;
 CREATE DATABASE DatingAppH2;
 USE DatingAppH2;
+
 									-- TABLES --
 CREATE TABLE account(
-    id INT AUTO_INCREMENT NOT NULL,
-    username VARCHAR(256) NOT NULL,
-    password VARCHAR(256) NOT NULL,
-    phone INT(11) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    address VARCHAR(35) NOT NULL,
-    city VARCHAR(35) NOT NULL,
-    secretKey VARCHAR(10) DEFAULT NULL,
-    profileId int DEFAULT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    city int,
+    username VARCHAR(255),
+    phone int,
+    email VARCHAR(255) CHARSET utf8,
+    address VARCHAR(30),
+    secretKey VARCHAR(10) DEFAULT NULL
 );
-CREATE TALBE profile(
-    id INT NOT NULL,
-    firstName VARCHAR(20) NOT NULL,
-    lastName VARCHAR(20) NOT NULL,
-    sex, ENUM ('male','female','other') NOT NULL,
-    text VARCHAR(300) NOT NULL,
-    activeStatus ENUM ('notSearching', 'isSearching', 'deactivated') NOT NULL,
-    appearance VARCHAR(40) NOT NULL,
-    rating int(5) NOT NULL DEFAULT 2,
-    creationDate DATE NOT NULL,
-    banLenght NOT NULL,
-    photosId INT NOT NULL,
-    
+
+CREATE TABLE profile(
+    id INT PRIMARY KEY,
+    prefsId int,
+    photosId int,
+    firstName VARCHAR(20),
+    lastName VARCHAR(20),
+    sex ENUM ('male', 'female', 'other'),
+    text VARCHAR(500),
+    activeStatus ENUM ('notSearching', 'isSearching', 'deactivated'),
+    appearance VARCHAR(30),
+  	rating INT(5) DEFAULT 2,
+    creationDate DATE DEFAULT NOW(),
+    banLength DATE DEFAULT NULL
 );
-CREATE TABLE preferences(
-    id INT NOT NULL,
-    sexuality ENUM ('male', 'female', 'other', 'all')
+
+CREATE TABLE prefs(
+	id INT PRIMARY KEY,
+    sexuality ENUM ('male', 'female', 'all')
 );
+
 CREATE TABLE photos(
-    id INT NOT NULL,
-    url1 VARCHAR DEFAULT NULL,
-    url2 VARCHAR DEFAULT NULL,
-    url3 VARCHAR DEFAULT NULL,
-    url4 VARCHAR DEFAULT NULL,
-    url5 VARCHAR DEFAULT NULL,
-    url6 VARCHAR DEFAULT NULL,
-    url7 VARCHAR DEFAULT NULL,
-    url8 VARCHAR DEFAULT NULL,
-    url9 VARCHAR DEFAULT NULL
+	ID INT PRIMARY KEY,
+    url1 VARCHAR(500) DEFAULT NULL,
+    url2 VARCHAR(500) DEFAULT NULL,
+    url3 VARCHAR(500) DEFAULT NULL,
+    url4 VARCHAR(500) DEFAULT NULL,
+    url5 VARCHAR(500) DEFAULT NULL,
+    url6 VARCHAR(500) DEFAULT NULL,
+    url7 VARCHAR(500) DEFAULT NULL,
+    url8 VARCHAR(500) DEFAULT NULL,
+    url9 VARCHAR(500) DEFAULT NULL
 );
-CREATE TALBE city(
-    zip INT(4) NOT NULL,
-    city varchar(30) NOT NULL
+
+CREATE TABLE city(
+	city INT PRIMARY KEY,
+    zipcode int(4)
 );
 
 									-- ALTERS --
 ALTER TABLE account
-ADD FOREIGN KEY (city) REFERENCES zipBy(city);
-ALTER TABLE profile
-ADD FOREIGN KEY (accountId) REFERENCES account(id);
-ALTER TABLE profile
-ADD FOREIGN KEY (photosId) REFERENCES photos(id);
+ADD FOREIGN KEY (city) 
+REFERENCES city(city);
 
+ALTER TABLE profile
+ADD FOREIGN KEY (id)
+REFERENCES account(id);
+
+ALTER TABLE prefs
+ADD FOREIGN KEY (id)
+REFERENCES profile(id);
+
+ALTER TABLE photos
+ADD FOREIGN KEY (id)
+REFERENCES profile(id);
